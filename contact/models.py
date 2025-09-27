18d5da5a346f4a0b36d517daa75e675a7405c7f8
Ederson Cruz <edersoncruz1337@gmail.com>
2025-09-27 13:11:19 -0300

    verbose name and field owner

--- contact/models.py
+++ contact/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=50)
     
     def __str__(self) -> str:
@@ -18,6 +23,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
